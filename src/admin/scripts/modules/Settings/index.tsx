166248ca67f924ac83cc95e30d9d66a0dd6a785f
Tomas Sychra <sychrat@gmail.com>
2021-12-07 22:59:35 +0100

    Settings form submit, backup

--- src/admin/scripts/modules/Settings/index.tsx
+++ src/admin/scripts/modules/Settings/index.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import _ from 'lodash';
 
-import { ROUTES, ROUTE_SUFFIX, TOASTS_TIMEOUT_DEFAULT } from '../../constants';
+import { TOASTS_TIMEOUT_DEFAULT } from '../../constants';
 import { useSettings } from '../../hooks/common';
+import { cmsSettingsObjectProps } from '../../types/modules';
 import SettingsForm from './SettingsForm';
 import { useToasts } from '../../hooks/common';
 import { Preloader } from '../../components/ui';
@@ -10,19 +13,27 @@ import { Preloader } from '../../components/ui';
 interface SettingsModuleProps {}
 
 const SettingsModule = ({}: SettingsModuleProps) => {
+	const { t } = useTranslation(['common', 'messages', 'form']);
 	const dispatch = useDispatch();
-
 	const { createToasts } = useToasts(dispatch);
-	const { Settings } = useSettings();
+	const {
+		Settings,
+		updateSettings,
+		reloadSettings,
+		settings_loading,
+		settings_error,
+	} = useSettings();
 
 	const formSubmitHandler = (data: any, e: any) => {
-		console.log('formSubmitHandler', data);
-
-		// createToasts({
-		// 	title: t('messages:success.itemCreated'),
-		// 	context: 'success',
-		// 	timeout: TOASTS_TIMEOUT_DEFAULT,
-		// });
+		const master: cmsSettingsObjectProps = _.cloneDeep(data);
+		updateSettings(master).then((response) => {
+			reloadSettings();
+			createToasts({
+				title: t('messages:success.newSettingsUpdated'),
+				context: 'success',
+				timeout: TOASTS_TIMEOUT_DEFAULT,
+			});
+		});
 	};
 
 	const formSubmitErrorHandler = (error: any, e: any) => console.warn(error, e);
