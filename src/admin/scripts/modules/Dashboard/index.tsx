2cfe09286e596193e00f9b7e9b0b19346d60c60c
Tomas Sychra <sychrat@gmail.com>
2021-12-23 14:46:33 +0100

    Posts to approve dashboard tile show conditions, backup

--- src/admin/scripts/modules/Dashboard/index.tsx
+++ src/admin/scripts/modules/Dashboard/index.tsx
@@ -3,7 +3,9 @@ import Stack from '@mui/material/Stack';
 import { isDesktop } from 'react-device-detect';
 import styled from 'styled-components';
 
+import { USER_LEVEL_NUMS } from '../../constants';
 import Tile from '../../components/DashboardTile';
+import { useSettings, useProfile } from '../../hooks/common';
 
 const StyledStack = styled(Stack)`
 	margin-bottom: 1rem;
@@ -12,10 +14,15 @@ const StyledStack = styled(Stack)`
 interface DashboardModuleProps {}
 
 const DashboardModule = ({}: DashboardModuleProps) => {
+	const { Settings } = useSettings();
+	const { Profile } = useProfile();
+	const show_postsToApprove =
+		Settings?.content_redactor_approval &&
+		Profile?.user_level > USER_LEVEL_NUMS.redactor;
 	return (
 		<>
 			<StyledStack spacing={2} direction={isDesktop ? 'row' : 'column'}>
-				<Tile.PostsToApprove />
+				{show_postsToApprove && <Tile.PostsToApprove />}
 				<Tile.PostsLast />
 				<Tile.UploadsLast />
 			</StyledStack>
