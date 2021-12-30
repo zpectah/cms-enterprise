4be8283e44a5eac26b3b27d2f3113c4787d0f0da
Tomas Sychra <sychrat@gmail.com>
2021-12-30 16:46:54 +0100

    BasketModule updates, backup

--- src/web/scripts/module/BasketModule.js
+++ src/web/scripts/module/BasketModule.js
@@ -3,39 +3,49 @@ import $ from 'jquery';
 import { storage } from '../../../../utils/utils';
 
 class BasketModule {
-	constructor(options) {
-		this.root = $(`[data-module="BasketModule"]`);
-
-		this.storage_items = storage.get('basket-items').split(',');
-
-		//
-		this.productItem = this.root.find('[data-component="ProductItem"]');
-
-		//
-		this.widget = this.root.find('[data-component="BasketWidget"]');
-		this.widgetList = this.widget.find('[data-component="BasketWidgetList"]');
-		this.widgetListItems = this.widget.find(
-			'[data-component="BasketWidgetItem"]',
+	constructor(
+		opt = {
+			storageKey: 'basket-items',
+		},
+		sel = {
+			root: 'BasketModule',
+			basketWidget: 'BasketWidget',
+			basketWidgetList: 'BasketWidgetList',
+			basketWidgetItem: 'BasketWidgetItem',
+			basketWidgetItemAdd: 'BasketAddTrigger',
+			basketWidgetItemRemove: 'BasketWidgetRemoveTrigger',
+			basketWidgetItemInput: 'BasketWidgetItemInput',
+			listItemProduct: 'ProductItem',
+			basketItemId: 'widgetProductItem_',
+		},
+	) {
+		this.root = $(`[data-module="${sel.root}"]`);
+		this.opt = opt;
+		this.sel = sel;
+		this.storage_items = storage.get(opt.storageKey);
+		this.basket_items = this.storage_items ? this.storage_items.split(',') : [];
+		this.widget = this.root.find(`[data-component="${sel.basketWidget}"]`);
+		this.widgetList = this.widget.find(
+			`[data-component="${sel.basketWidgetList}"]`,
 		);
-
-		//
-		this.storage_items.map((item) => {
+		this.basket_items.map((item) => {
 			const id = item.split(':')[0];
 			const value = item.split(':')[1];
-
 			this.addHandler(id, value);
 		});
-
 		this.initEvents();
 	}
 
 	initEvents() {
-		this.addTrigger = this.root.find('[data-component="BasketAddTrigger"]');
-		this.removeTrigger = this.root.find(
-			'[data-component="BasketRemoveTrigger"]',
+		this.addTrigger = this.root.find(
+			`[data-component="${this.sel.basketWidgetItemAdd}"]`,
 		);
-		this.widgetItemInput = this.root.find('[data-component="BasketItemInput"]');
-
+		this.removeTrigger = this.root.find(
+			`[data-component="${this.sel.basketWidgetItemRemove}"]`,
+		);
+		this.widgetItemInput = this.root.find(
+			`[data-component="${this.sel.basketWidgetItemInput}"]`,
+		);
 		this.addTrigger.off().on('click', (e) => {
 			e.preventDefault();
 			this.addHandler(e.target.dataset.id);
@@ -52,19 +62,23 @@ class BasketModule {
 
 	updateStorageData() {
 		let arr = [];
+		let sel = this.sel;
 		this.root
-			.find('[data-component="BasketWidgetItem"]')
+			.find(`[data-component="${this.sel.basketWidgetItem}"]`)
 			.each(function (index, item) {
 				let id = $(item).data('id');
-				let count = $(item).find('[data-component="BasketItemInput"]').val();
+				let input = $(item).find(
+					`[data-component="${sel.basketWidgetItemInput}"]`,
+				);
+				let count = input.val();
 				arr.push(`${id}:${count}`);
 			});
-		storage.set('basket-items', arr.toString());
+		storage.set(this.opt.storageKey, arr.toString());
 	}
 
 	updateProductItem(id, method = 'update') {
 		const el = this.root.find(
-			`[data-component="ProductItem"][data-component-id="${id}"]`,
+			`[data-component="${this.sel.listItemProduct}"][data-component-id="${id}"]`,
 		);
 		if (method === 'update') {
 			el.addClass('in-basket');
@@ -74,25 +88,25 @@ class BasketModule {
 	}
 
 	addHandler(id, value = 1) {
-		const item = this.widgetList.find(`#widgetProductItem_${id}`);
+		const item = this.widgetList.find(`#${this.sel.basketItemId}${id}`);
 		if (item.length === 0) {
 			this.widgetList.append(
 				`<div 
-					id="widgetProductItem_${id}" 
-					data-component="BasketWidgetItem"
+					id="${this.sel.basketItemId}${id}" 
+					data-component="${this.sel.basketWidgetItem}"
 					data-id="${id}"
 				>
 					<input 
 						type="number" 
-						id="widgetProductItem_value_${id}" 
-						data-component="BasketItemInput"
+						id="${this.sel.basketItemId}value_${id}" 
+						data-component="${this.sel.basketWidgetItemInput}"
 						value="${value}" 
 						data-id="${id}" 
 					/>
 					product #${id}
 					<button 
 						type="button" 
-						data-component="BasketRemoveTrigger" 
+						data-component="${this.sel.basketWidgetItemRemove}" 
 						data-id="${id}"
 					>
 						Remove
@@ -100,9 +114,9 @@ class BasketModule {
 				</div>`,
 			);
 		} else {
-			let e_value = item.find(`#widgetProductItem_value_${id}`).val();
+			let e_value = item.find(`#${this.sel.basketItemId}value_${id}`).val();
 			e_value = Number(e_value) + Number(value);
-			this.widgetList.find(`#widgetProductItem_value_${id}`).val(e_value);
+			this.widgetList.find(`#${this.sel.basketItemId}value_${id}`).val(e_value);
 		}
 		this.updateProductItem(id);
 		this.updateStorageData();
@@ -124,4 +138,4 @@ class BasketModule {
 	}
 }
 
-new BasketModule({});
+new BasketModule();
